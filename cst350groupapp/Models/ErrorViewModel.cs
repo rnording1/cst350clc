10d8d9cce2b43e258023fe5a19f505becb80c179
Robert Nording <robert.nording@wixcorp.com>
2024-10-14 12:32:07 -0700

    Initial commit

---
+++ cst350groupapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cst350groupapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
