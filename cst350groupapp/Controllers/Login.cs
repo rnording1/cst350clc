3a325e377f21686b782b2fc1d49f3ce2de9e474c
Robert Nording <robert.nording@wixcorp.com>
2024-10-16 14:14:58 -0700

    User Model

---
+++ cst350groupapp/Controllers/Login.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cst350groupapp.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ cst350groupapp/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace cst350groupapp.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
+        public string Groups { get; set; }
+    }
+}
