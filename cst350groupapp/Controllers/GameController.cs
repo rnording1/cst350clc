fb4d3d13bdc013e8529e0809bcc632309066bad4
trevormarr <tmarr2@my.gcu.edu>
2024-10-18 09:54:46 -0700

    95% done with my part. Just waiting on login and session

---
+++ cst350groupapp/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace cst350groupapp.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult StartGame()
+        {
+            // Check if the session variable "User" exists and is not null
+            var userJson = Session["User"] as string;
+            if (string.IsNullOrEmpty(userJson))
+            {
+                // If the user is not logged in, redirect them to the login page
+                return RedirectToAction("Login", "Account"); // Change "Account" to the correct controller handling login
+            }
+
+            // User is logged in, proceed to display the StartGame view
+            return View();
+        }
+    }  
+}
