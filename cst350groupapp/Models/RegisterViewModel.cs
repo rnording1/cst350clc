e262b81cd9b483e8e2b299db67499158b6667a98
Robert Nording <robert.nording@wixcorp.com>
2024-10-30 18:35:20 -0700

    changing field validation

--- cst350groupapp/Models/RegisterViewModel.cs
+++ cst350groupapp/Models/RegisterViewModel.cs
@@ -1,13 +1,42 @@
-﻿namespace cst350groupapp.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace cst350groupapp.Models
 {
     public class RegisterViewModel
     {
+        [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Gender")]
         public int Sex { get; set; }
+
+        [Required]
+        [DisplayName("Age")]
+        [Range(1, 120)]
         public int Age { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Username")]
+        [StringLength(20, MinimumLength = 4)]
         public string Username { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        // at least 8 characters long, contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
